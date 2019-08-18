0b4a64d1f64e3aa99ae8942b335686b7b5fe363e
calum <cbai5778@uni.sydney.edu.au>
2019-08-18 13:37:03 +1000

    working install file

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install pandoc mupdf texlive-extra-utils texlive-fonts-recommended
+sudo apt install pandoc mupdf texlive-extra-utils texlive-fonts-recommended texlive-pictures texlive-generic-recommended texlive-latex-extra
 sudo cp notetaking /usr/local/bin
 sudo cp notetaking.latex /usr/share/pandoc/data/templates/notetaking.latex
 
