b5dacc7af1667a4524228fe3871ebb21e28c53ef
calum <cbai5778@uni.sydney.edu.au>
2020-03-29 20:19:58 +1100

    merging with master

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo apt install scrot pandoc mupdf texlive-extra-utils texlive-fonts-recommended texlive-pictures texlive-generic-recommended texlive-latex-extra
-sudo cp screenshot /usr/local/bin
-sudo cp notetaking /usr/local/bin
-sudo cp notetaking.latex /usr/share/pandoc/data/templates/notetaking.latex
-
